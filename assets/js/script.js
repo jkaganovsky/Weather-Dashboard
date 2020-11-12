3b35781c2ed35f0a320f6c71a1137a33ab3f0eb3
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2020-11-11 23:13:36 -0800

    Added user input to append to ul.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,49 @@
-// Performing GET requests to the openweather API and logging the response to the console
-// Current weather API AJAX GET request
+// Get user input from search-input
+$(document).ready(function(){
+  // Get value on button click
+  $("#city-search").click(function(){
+      var str = $("#search-input").val();
+      console.log(str);
 
-var cityname = "Seattle";
-var statecode = "98125";
-console.log(`https://api.openweathermap.org/data/2.5/weather?q=${cityname},${statecode}&appid=fd427a830aad982e864b2dfe048a7248`);
+  $("ul").append("<li class=list-group-item>" + str + "</li>")
 
-$.ajax({
-    url: "https://api.openweathermap.org/data/2.5/weather?q=${cityname},${statecode}&appid=fd427a830aad982e864b2dfe048a7248",
-    method: "GET"
-  }).then(function(response) {
-    console.log(response);
-  });
+  // Performing GET requests to the openweather API and logging the response to the console
+  // Current weather API AJAX GET request
 
-// 5 Day Daily Forecast API AJAX GET request
-$.ajax({
-url: "https://api.openweathermap.org/data/2.5/forecast?q=Seattle,98125&appid=fd427a830aad982e864b2dfe048a7248",
-method: "GET"
-}).then(function(response) {
-console.log(response);
-// console.log(response.city.name);
-});
+  var cityname = "Seattle";
+  var statecode = "98125";
+  console.log(`https://api.openweathermap.org/data/2.5/weather?q=${cityname},${statecode}&appid=fd427a830aad982e864b2dfe048a7248`);
 
-// UV Index API AJAX GET request
-$.ajax({
-    url: "http://api.openweathermap.org/data/2.5/uvi?lat=47.6062&lon=-122.3321&appid=fd427a830aad982e864b2dfe048a7248",
+  $.ajax({
+      url: `https://api.openweathermap.org/data/2.5/weather?q=${cityname},${statecode}&appid=fd427a830aad982e864b2dfe048a7248`,
+      method: "GET"
+    }).then(function(response) {
+      console.log(response);
+    });
+
+
+  // 5 Day Daily Forecast API AJAX GET request
+  $.ajax({
+    url: "https://api.openweathermap.org/data/2.5/forecast?q=Seattle,98125&appid=fd427a830aad982e864b2dfe048a7248",
     method: "GET"
     }).then(function(response) {
     console.log(response);
-    });
+  // console.log(response.city.name);
+  });
+
+  // UV Index API AJAX GET request
+  $.ajax({
+      url: "http://api.openweathermap.org/data/2.5/uvi?lat=47.6062&lon=-122.3321&appid=fd427a830aad982e864b2dfe048a7248",
+      method: "GET"
+      }).then(function(response) {
+      console.log(response);
+      });
+
+  });
+
+});
+
+
 
 
 // Event listener for search button element
